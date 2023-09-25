86d4ca7024521acaf5b75a98002ba2d24938e831
Tagir <manualqaengineer@mail.ru>
2023-09-25 12:24:38 +0300

    Класс имен для выбора поля баг-репорта

---
+++ start/names.py
@@ -0,0 +1,6 @@
+class Names:
+    title = {'title', 't', 'заголовок', 'з'}
+    preconditions = {'preconditions', 'pc', 'p', 'предусловие', 'предусловия', 'п'}
+    actual_result = {'actual result', 'ar', 'a', 'фактический результат', 'ф', 'фр'}
+    expected_result = {'expected result', 'er', 'e', 'ожидаемый результат', 'ор', 'о'}
+
